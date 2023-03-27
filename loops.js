06d4d87c4dd99f98721632fc37dd3a4436525b18
jeffreyheideman <jeffrey_heideman@hotmail.com>
2023-03-27 12:21:54 +0200

    De bonusopdracht afgerond

--- loops.js
+++ loops.js
@@ -106,19 +106,16 @@ for (let i = 0; i < 10; i++) {
 // etc.
 // ==========================================
 
-for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0) {
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 === 0) {
         console.log("Fizz")
     }
-
     else if (i % 5 === 0) {
         console.log("Buzz")
     }
-
-    else if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log("FizzBuzz")
-    }
-
     else {
         console.log(i)
     }
